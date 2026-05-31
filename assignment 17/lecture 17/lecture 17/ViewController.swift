0ef7197f57b53eadef5e8f3ac78250de95498bad
NatiaMinasiani <natiaminasiani@gmail.com>
2026-05-31 15:43:47 +0400

    added initial files

---
+++ assignment 17/lecture 17/lecture 17/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lecture 17
+//
+//  Created by naat minasiani on 31/05/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
