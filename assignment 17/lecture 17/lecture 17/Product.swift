343eb9a3901c48710bc940d5bd3572883216de0e
NatiaMinasiani <natiaminasiani@gmail.com>
2026-05-31 15:54:43 +0400

    added assets and files

---
+++ assignment 17/lecture 17/lecture 17/Product.swift
@@ -0,0 +1,8 @@
+//
+//  Product.swift
+//  lecture 17
+//
+//  Created by naat minasiani on 31/05/2026.
+//
+
+import Foundation
---
+++ assignment 17/lecture 17/lecture 17/ProductCollectionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProductCollectionViewController.swift
+//  lecture 17
+//
+//  Created by naat minasiani on 31/05/2026.
+//
+
+import UIKit
+
+class ProductCollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
