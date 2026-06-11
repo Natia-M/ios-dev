cd1877c1a3fc38efe90388a4103b422d213ab245
NatiaMinasiani <natiaminasiani@gmail.com>
2026-06-12 00:21:16 +0400

    created initial files for lect18

---
+++ lecture18/lecture18/lecture18App.swift
@@ -0,0 +1,17 @@
+//
+//  lecture18App.swift
+//  lecture18
+//
+//  Created by naat minasiani on 12/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct lecture18App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
